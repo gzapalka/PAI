5b4b5107c27c3649dde15b9d8e91fc02201ba5f8
Gabriela Zapała <gabzapala@gmail.com>
2021-11-21 20:56:03 +0100

    add css for login page

---
+++ public/views/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<head>
+  <link rel="stylesheet" href="../css/styles.css">
+  <title>SimpleBudget</title>
+</head>
+<body>
+  <div class="container">
+    <div class="left-container">
+      <img src="../img/Logo2.png">
+    </div>
+    <div class="right-container">
+      <div class="login-container">
+        <div class="logo">
+          <h1>SimpleBudget</h1>
+        </div>
+        <div class="login-panel">
+          <form class="login" action="login" method="POST">
+            <input class="email input-style" name="email" type="text" placeholder="email">
+            <input class="pwd input-style" name="password" type="password" placeholder="password">
+            <button class="submit login-button-style" type="submit">login</button>
+            <button class="register login-button-style" type="">join us!</button>
+            <div class="forgot">
+              <a href="#">Forgot password?</a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
