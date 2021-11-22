f000b7d5fedaa75ecaf9c9e38dd41f4579f1d623
Gabriela Zapała <gabzapala@gmail.com>
2021-11-22 21:15:16 +0100

    add register page css

---
+++ public/views/register.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<head>
+  <link rel="stylesheet" href="../css/styles.css">
+  <title>SimpleBudget</title>
+</head>
+<body>
+  <div class="container">
+    <div class="left-container">
+      <img src="../img/Logo2.png">
+    </div>
+    <div class="right-container">
+      <div class="login-container">
+        <div class="logo">
+          <h1>SimpleBudget</h1>
+        </div>
+        <div class="login-panel">
+          <form class="register" action="register" method="POST">
+            <input class="email input-style" name="email" type="text" placeholder="email">
+            <input class="username input-style" name="username" type="text" placeholder="username">
+            <input class="pwd input-style" name="password" type="text" placeholder="password">
+            <input class="rpwd input-style" name="repeat password" type="text" placeholder="repeat password">
+            <button class="submit login-button-style" type="submit">join us!</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
