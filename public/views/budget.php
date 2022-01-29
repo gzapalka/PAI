82f186a0e3212b56bf005594c872879f937c419c
Gabriela Zapała <gabzapala@gmail.com>
2022-01-29 20:06:37 +0100

    - checkstyle

--- public/views/budget.php
+++ public/views/budget.php
@@ -4,7 +4,6 @@
     <title>SimpleBudget</title>
 </head>
 <body>
-<html>
     <header>
         <h1>Simple Budget</h1>
         <h1></h1>
@@ -21,23 +20,22 @@
         <contact_info>
             <h2>About us</h2>
             <social_media>
-                <img src="../img/fb.png">
+                <img src="../img/fb.png" alt="FB">
                 <h2>Facebook</h2>
             </social_media>
             <h2>123 street City, 09-732</h2>
             <h2>Contact</h2>
             <social_media>
-                <img src="../img/ig.png">
+                <img src="../img/ig.png" alt="IG">
                 <h2>Instagram</h2>
             </social_media>
             <h2>+48 025 657 129</h2>
             <h2>Term & Policy</h2>
             <social_media>
-                <img src="../img/tt.png">
+                <img src="../img/tt.png" alt="TT">
                 <h2>Twitter</h2>
             </social_media>
             <h2>simplebudget@mail.com</h2>
         </contact_info>
     </footer>
 </body>
-</html>
