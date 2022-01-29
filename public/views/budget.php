4c2a3caf0729d0ebad9180f0ef51179cda73b244
Gabriela Zapała <gabzapala@gmail.com>
2022-01-29 20:03:16 +0100

    - add footer css for budget page

--- public/views/budget.php
+++ public/views/budget.php
@@ -11,6 +11,33 @@
         <button>Log out</button>
     </header>
     <content></content>
-    <footer></footer>
+    <footer>
+        <money_to_assign>
+            <h1>Money to assign:</h1>
+            <amount>0.00</amount>
+            <h1>zł</h1>
+        </money_to_assign>
+        <h1></h1>
+        <contact_info>
+            <h2>About us</h2>
+            <social_media>
+                <img src="../img/fb.png">
+                <h2>Facebook</h2>
+            </social_media>
+            <h2>123 street City, 09-732</h2>
+            <h2>Contact</h2>
+            <social_media>
+                <img src="../img/ig.png">
+                <h2>Instagram</h2>
+            </social_media>
+            <h2>+48 025 657 129</h2>
+            <h2>Term & Policy</h2>
+            <social_media>
+                <img src="../img/tt.png">
+                <h2>Twitter</h2>
+            </social_media>
+            <h2>simplebudget@mail.com</h2>
+        </contact_info>
+    </footer>
 </body>
 </html>
