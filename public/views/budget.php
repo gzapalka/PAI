2a140cf374072bba42567a29a37ad17f909b4c8e
Gabriela Zapała <gabzapala@gmail.com>
2022-01-29 18:15:55 +0100

    - add footer css for budget page

---
+++ public/views/budget.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="../css/budgets.css">
+    <title>SimpleBudget</title>
+</head>
+<body>
+<html>
+    <header>
+        <h1>Simple Budget</h1>
+        <h1></h1>
+        <button>Log out</button>
+    </header>
+    <content></content>
+    <footer></footer>
+</body>
+</html>
