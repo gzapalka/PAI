c7364dce984494d3fc312ba6ef57d2b61bfb6c4a
Gabriela Zapała <gabzapala@gmail.com>
2022-01-30 10:33:23 +0100

    - add basic css for content

--- public/views/budget.php
+++ public/views/budget.php
@@ -4,38 +4,189 @@
     <title>SimpleBudget</title>
 </head>
 <body>
-    <header>
-        <h1>Simple Budget</h1>
-        <h1></h1>
-        <button>Log out</button>
-    </header>
-    <content></content>
-    <footer>
-        <money_to_assign>
-            <h1>Money to assign:</h1>
-            <amount>0.00</amount>
-            <h1>zł</h1>
-        </money_to_assign>
-        <h1></h1>
-        <contact_info>
-            <h2>About us</h2>
-            <social_media>
-                <img src="../img/fb.png" alt="FB">
-                <h2>Facebook</h2>
-            </social_media>
-            <h2>123 street City, 09-732</h2>
-            <h2>Contact</h2>
-            <social_media>
-                <img src="../img/ig.png" alt="IG">
-                <h2>Instagram</h2>
-            </social_media>
-            <h2>+48 025 657 129</h2>
-            <h2>Term & Policy</h2>
-            <social_media>
-                <img src="../img/tt.png" alt="TT">
-                <h2>Twitter</h2>
-            </social_media>
-            <h2>simplebudget@mail.com</h2>
-        </contact_info>
-    </footer>
+<header>
+    <h1>Simple Budget</h1>
+    <h1></h1>
+    <button>Log out</button>
+</header>
+<content>
+    <bookmarks>
+        <bookmark style="background-color:#E9E4E4;">Budget</bookmark>
+        <bookmark style="background-color:#F8F4F4;">Statistic</bookmark>
+        <bookmark style="background-color:#F3E6D1;">Transaction</bookmark>
+        <bookmark style="background-color:#D5E6F2;">Settings</bookmark>
+    </bookmarks>
+    <div class="bookmark_container">
+        <div class="break"></div>
+        <table class="budget_table">
+            <col><col><col><col><col>
+            <thead>
+                <tr>
+                    <th class="category_name">Category</th>
+                    <th class="category_name">Assigned</th>
+                    <th class="category_name">Spent</th>
+                    <th class="category_name">Left</th>
+                    <th class="category_name">%</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <table class="subtable">
+                        <thead>
+                        <tr>
+                            <th class="subcategory_name">Home</th>
+                        </tr>
+                        </thead>
+                        <tr>
+                            <td class="sub_subcategory_name">Rent</td>
+                            <td class="sub_subcategory_assign">650.00</td>
+                            <td class="sub_subcategory_spent">650.00</td>
+                            <td class="sub_subcategory_left">0.00</td>
+                            <td class="sub_subcategory_percent">100</td>
+                        </tr>
+                        <tr>
+                            <td class="sub_subcategory_name">Water</td>
+                            <td class="sub_subcategory_assign">650.00</td>
+                            <td class="sub_subcategory_spent">650.00</td>
+                            <td class="sub_subcategory_left">0.00</td>
+                            <td class="sub_subcategory_percent">100</td>
+                        </tr>
+                        <tr>
+                            <td class="sub_subcategory_name">Energy</td>
+                            <td class="sub_subcategory_assign">650.00</td>
+                            <td class="sub_subcategory_spent">650.00</td>
+                            <td class="sub_subcategory_left">0.00</td>
+                            <td class="sub_subcategory_percent">100</td>
+                        <tr>
+                        <tr>
+                            <td class="sub_subcategory_name">Groceries</td>
+                            <td class="sub_subcategory_assign">650.00</td>
+                            <td class="sub_subcategory_spent">650.00</td>
+                            <td class="sub_subcategory_left">0.00</td>
+                            <td class="sub_subcategory_percent">100</td>
+                        </tr>
+                        <tr>
+                            <td class="sub_subcategory_name">Internet</td>
+                            <td class="sub_subcategory_assign">650.00</td>
+                            <td class="sub_subcategory_spent">650.00</td>
+                            <td class="sub_subcategory_left">0.00</td>
+                            <td class="sub_subcategory_percent">100</td>
+                        </tr>
+                    </table>
+                </tr>
+                <tr>
+                    <table class="subtable">
+                        <thead>
+                        <tr>
+                            <th class="subcategory_name">Transport</th>
+                        </tr>
+                        </thead>
+                        <tr>
+                            <td class="sub_subcategory_name">Car maintenance</td>
+                            <td class="sub_subcategory_assign">1200.00</td>
+                            <td class="sub_subcategory_spent">200.00</td>
+                            <td class="sub_subcategory_left">1000.00</td>
+                            <td class="sub_subcategory_percent">16</td>
+                        </tr>
+                        <tr>
+                            <td class="sub_subcategory_name">Fuel</td>
+                            <td class="sub_subcategory_assign">650.00</td>
+                            <td class="sub_subcategory_spent">650.00</td>
+                            <td class="sub_subcategory_left">0.00</td>
+                            <td class="sub_subcategory_percent">100</td>
+                        </tr>
+                        <tr>
+                            <td class="sub_subcategory_name">Bus Ticket</td>
+                            <td class="sub_subcategory_assign">650.00</td>
+                            <td class="sub_subcategory_spent">650.00</td>
+                            <td class="sub_subcategory_left">0.00</td>
+                            <td class="sub_subcategory_percent">100</td>
+                        <tr>
+                    </table>
+                </tr>
+                <tr>
+                    <table class="subtable">
+                        <thead>
+                        <tr>
+                            <th class="subcategory_name">Just for fun</th>
+                        </tr>
+                        </thead>
+                        <tr>
+                            <td class="sub_subcategory_name">Netflix</td>
+                            <td class="sub_subcategory_assign">1200.00</td>
+                            <td class="sub_subcategory_spent">200.00</td>
+                            <td class="sub_subcategory_left">1000.00</td>
+                            <td class="sub_subcategory_percent">16</td>
+                        </tr>
+                        <tr>
+                            <td class="sub_subcategory_name">Dinning Out</td>
+                            <td class="sub_subcategory_assign">650.00</td>
+                            <td class="sub_subcategory_spent">650.00</td>
+                            <td class="sub_subcategory_left">0.00</td>
+                            <td class="sub_subcategory_percent">100</td>
+                        </tr>
+                        <tr>
+                            <td class="sub_subcategory_name">Clubbing</td>
+                            <td class="sub_subcategory_assign">650.00</td>
+                            <td class="sub_subcategory_spent">650.00</td>
+                            <td class="sub_subcategory_left">0.00</td>
+                            <td class="sub_subcategory_percent">100</td>
+                        <tr>
+                        <tr>
+                            <td class="sub_subcategory_name">Gaming</td>
+                            <td class="sub_subcategory_assign">650.00</td>
+                            <td class="sub_subcategory_spent">650.00</td>
+                            <td class="sub_subcategory_left">0.00</td>
+                            <td class="sub_subcategory_percent">100</td>
+                        <tr>
+                    </table>
+                </tr>
+                <tr>
+                    <table class="subtable">
+                        <thead>
+                        <tr>
+                            <th class="subcategory_name">Education</th>
+                        </tr>
+                        </thead>
+                        <tr>
+                            <td class="sub_subcategory_name">School Fees</td>
+                            <td class="sub_subcategory_assign">1200.00</td>
+                            <td class="sub_subcategory_spent">200.00</td>
+                            <td class="sub_subcategory_left">1000.00</td>
+                            <td class="sub_subcategory_percent">16</td>
+                        </tr>
+                    </table>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</content>
+<footer>
+    <money_to_assign>
+        <h1>Money to assign:</h1>
+        <amount>0.00</amount>
+        <h1>zł</h1>
+    </money_to_assign>
+    <h1></h1>
+    <contact_info>
+        <h2>About us</h2>
+        <social_media>
+            <img src="../img/fb.png" alt="FB">
+            <h2>Facebook</h2>
+        </social_media>
+        <h2>123 street City, 09-732</h2>
+        <h2>Contact</h2>
+        <social_media>
+            <img src="../img/ig.png" alt="IG">
+            <h2>Instagram</h2>
+        </social_media>
+        <h2>+48 025 657 129</h2>
+        <h2>Term & Policy</h2>
+        <social_media>
+            <img src="../img/tt.png" alt="TT">
+            <h2>Twitter</h2>
+        </social_media>
+        <h2>simplebudget@mail.com</h2>
+    </contact_info>
+</footer>
 </body>
