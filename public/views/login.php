194c891787b4b7d111645dd64b53b067ac38b248
Gabriela Zapała <gabzapala@gmail.com>
2022-01-30 22:09:20 +0100

    - add js validation for logging

--- public/views/login.php
+++ public/views/login.php
@@ -3,10 +3,10 @@
   <link rel="stylesheet" href="../css/styles.css">
   <title>SimpleBudget</title>
 </head>
-<body>
+<body onload='login.email.focus()'>
   <div class="container">
     <div class="left-container">
-      <img src="../img/Logo2.png">
+      <img src="../img/Logo2.png" alt="Logo">
     </div>
     <div class="right-container">
       <div class="login-container">
@@ -14,17 +14,44 @@
           <h1>SimpleBudget</h1>
         </div>
         <div class="login-panel">
-          <form class="login" action="login" method="POST">
-            <input class="email input-style" name="email" type="text" placeholder="email">
-            <input class="pwd input-style" name="password" type="password" placeholder="password">
-            <button class="submit login-button-style" type="submit">login</button>
+          <form name="login" class="login" action="login" method="POST">
+              <label>
+                  <input class="email input-style" name="email" id="email" type="text" placeholder="email" onblur="ValidateEmail(document.login.email)">
+              </label>
+                  <input class="pwd input-style" id="password" name="password" type="password" placeholder="password" onblur="enableLogin()">
+            <button class="submit login-button-style" type="submit" id="login-button">login</button>
             <button class="register login-button-style" type="">join us!</button>
-            <div class="forgot">
-              <a href="#">Forgot password?</a>
-            </div>
           </form>
         </div>
       </div>
     </div>
   </div>
+
+<script>
+    function ValidateEmail(inputText)
+    {
+        const mailFormat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+        if(inputText.value.match(mailFormat))
+        {
+            enableLogin();
+            document.login.email.focus();
+            return true;
+        }
+        else
+        {
+            document.getElementById('email').value = ''
+            alert("You have entered an invalid email address!");
+            document.login.email.focus();
+            return false;
+        }
+    }
+
+    function enableLogin(){
+        if (document.forms["login"]["email"].value != "" && document.forms["login"]["password"].value != "") {
+            document.getElementById("login-button").disabled = false;
+        } else {
+            document.getElementById("login-button").disabled = true;
+        }
+    }
+</script>
 </body>
--- public/views/register.php
+++ public/views/register.php
@@ -25,4 +25,41 @@
       </div>
     </div>
   </div>
+
+<script>
+    function ValidateEmail(inputText)
+    {
+        const mailFormat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+        if(inputText.value.match(mailFormat))
+        {
+            document.login.email.focus();
+            return true;
+        }
+        else
+        {
+            alert("You have entered an invalid email address!");
+            document.login.email.focus();
+            return false;
+        }
+    }
+
+    function passwordChanged(inputText) {
+        const strongRegex = new RegExp("^(?=.{14,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
+        const mediumRegex = new RegExp("^(?=.{10,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
+        const enoughRegex = new RegExp("(?=.{8,}).*", "g");
+        const pwd = document.getElementById("password");
+        if (pwd.value.length === 0) {
+            strength.innerHTML = 'Type Password';
+        } else if (false === enoughRegex.test(pwd.value)) {
+            strength.innerHTML = 'More Characters';
+        } else if (strongRegex.test(pwd.value)) {
+            strength.innerHTML = '<span style="color:green">Strong!</span>';
+        } else if (mediumRegex.test(pwd.value)) {
+            strength.innerHTML = '<span style="color:orange">Medium!</span>';
+        } else {
+            strength.innerHTML = '<span style="color:red">Weak!</span>';
+        }
+    }
+
+</script>
 </body>
