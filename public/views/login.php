d18ec9acf1b15eb7570d88e8865060a61d4293b2
Gabriela Zapała <gabzapala@gmail.com>
2022-01-30 23:19:32 +0100

    - add filled fields validator

--- public/views/login.php
+++ public/views/login.php
@@ -3,7 +3,7 @@
   <link rel="stylesheet" href="../css/styles.css">
   <title>SimpleBudget</title>
 </head>
-<body onload='login.email.focus()'>
+<body>
   <div class="container">
     <div class="left-container">
       <img src="../img/Logo2.png" alt="Logo">
@@ -19,7 +19,7 @@
                   <input class="email input-style" name="email" id="email" type="text" placeholder="email" onblur="ValidateEmail(document.login.email)">
               </label>
                   <input class="pwd input-style" id="password" name="password" type="password" placeholder="password" onblur="enableLogin()">
-            <button class="submit login-button-style" type="submit" id="login-button">login</button>
+            <button class="submit login-button-style" type="submit" id="login-button" disabled="disabled">login</button>
             <button class="register login-button-style" type="">join us!</button>
           </form>
         </div>
@@ -28,6 +28,41 @@
   </div>
 
 <script>
+    let inputs = document.querySelectorAll('input');
+    let buttonSend = document.getElementById('login-button');
+
+    const enableRegisterButton = function () {
+        let email = document.login.email;
+        let password = document.login.password;
+        alert(email, password);
+    }
+
+    let inputValidator = {
+        "email": false,
+        "password": false
+    }
+
+    inputs.forEach((input) => {
+        input.addEventListener('input', () => {
+            let name = event.target.getAttribute('name');
+            if (event.target.value.length > 0) {
+                inputValidator[name] = true;
+            } else {
+                inputValidator[name] = false;
+            };
+
+            let allTrue = Object.keys(inputValidator).every((item) => {
+                return inputValidator[item] === true
+            });
+
+            if (allTrue) {
+                buttonSend.disabled = false;
+            } else {
+                buttonSend.disabled = true;
+            }
+        })
+    })
+
     function ValidateEmail(inputText)
     {
         const mailFormat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
