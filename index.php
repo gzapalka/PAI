1e3f87935e6fdb153b8e4f81ac9c5d2bfd246bba
Gabriela Zapała <gabzapala@gmail.com>
2022-02-01 01:13:35 +0100

    - statistic page v1

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ $path = parse_url( $path, PHP_URL_PATH);
 Router::get('', 'DefaultController');
 Router::get('register', "DefaultController");
 Router::get('login', "DefaultController");
+Router::get('statistic', "DefaultController");
 Router::get('register_user', "DefaultController");
 Router::post('register_user', 'SecurityController');
 Router::post("login_user", 'SecurityController');
--- public/views/statistic.php
+++ public/views/statistic.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <head>
-    <link rel="stylesheet" href="public/css/budgets.css">
+    <link rel="stylesheet" href="../css/budgets.css">
     <title>SimpleBudget</title>
 </head>
-<body>
+<body style="overflow: auto">
 <header>
     <h1>Simple Budget</h1>
     <h1></h1>
@@ -31,19 +31,19 @@
         <div class="statistic_left_container">
             <div class="statistic">
                 <div class="statistic_img">
-                    <img src="public/img/expenditures_income.jpg" alt="EI">
+                    <img class="ei" src="../img/expenditures_income.jpg" alt="EI">
                 </div>
             </div>
             <div class="statistic">
                 <div class="statistic_img">
-                    <img src="public/img/expenditures_income.jpg" alt="EI">
+                    <img class="ei" src="../img/expenditures_income.jpg" alt="EI">
                 </div>
             </div>
         </div>
         <div class="statistic_right_container">
             <div class="statistic">
                 <div class="statistic_img">
-                    <img src="public/img/months.jpg" alt="EI">
+                    <img class="ei" src="../img/months.jpg" alt="EI">
                 </div>
             </div>
         </div>
@@ -51,29 +51,23 @@
 </content>
 <footer>
     <money_to_assign>
-        <h1>Money to assign:</h1>
-        <amount>0.00</amount>
+        <h1>Money: </h1>
+        <h1 style="color: #555555">0.00</h1>
         <h1>zł</h1>
     </money_to_assign>
     <h1></h1>
     <contact_info>
         <h2>About us</h2>
-        <social_media>
-            <img src="public/img/fb.png" alt="FB">
+        <div class="social_media">
+            <img class="social_media_img" src="../img/fb.png" alt="FB">
             <h2>Facebook</h2>
-        </social_media>
+        </div class="social_media">
         <h2>123 street City, 09-732</h2>
         <h2>Contact</h2>
-        <social_media>
-            <img src="public/img/ig.png" alt="IG">
+        <div class="social_media">
+            <img class="social_media_img" src="../img/ig.png" alt="IG">
             <h2>Instagram</h2>
-        </social_media>
-        <h2>+48 025 657 129</h2>
-        <h2>Term & Policy</h2>
-        <social_media>
-            <img src="public/img/tt.png" alt="TT">
-            <h2>Twitter</h2>
-        </social_media>
+        </div>
         <h2>simplebudget@mail.com</h2>
     </contact_info>
 </footer>
